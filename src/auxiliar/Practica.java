ae21eb8a8412c73a0c1210050917aeeec6a44194
Ivancito Mañana <Ivancito Maana@DESKTOP-1PIPS59>
2017-11-28 08:24:56 +0000

    ordenar clasificacion

--- src/auxiliar/Practica.java
+++ src/auxiliar/Practica.java
@@ -361,4 +361,17 @@ public class Practica {
 		}
 		return puntosEquipo;
 	}
+
+	public void ordenaClasificacion(int[] numeros, String[] equipos) {
+		for (int i = 0; i < numeros.length - 1; i++)
+			for (int j = i + 1; j < numeros.length; j++)
+				if (numeros[i] < numeros[j]) {
+					int aux = numeros[i];
+					numeros[i] = numeros[j];
+					numeros[j] = aux;
+					String aux2 = equipos[i];
+					equipos[i] = equipos[j];
+					equipos[j] = aux2;
+				}
+	}
 }
